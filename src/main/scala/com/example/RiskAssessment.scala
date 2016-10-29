f6f6678e4d33cf025191362817ccdd826c267ecc
shigemk2 <i.am.shige@gmail.com>
2016-10-30 06:43:16 +0900

    Add case classes

--- src/main/scala/com/example/RiskAssessment.scala
+++ src/main/scala/com/example/RiskAssessment.scala
@@ -10,4 +10,27 @@ object RiskAssessmentDriver extends CompletableApp(2) {
   implicit val timeout = Timeout(5 seconds)
 }
 
+case class AttacheDocument(documentText: String)
+case class ClassifyRisk()
+case class RiskClasified(classification: String)
 
+case class Document(documentText: Option[String]) {
+  if (documentText.isDefined) {
+    val text = documentText.get
+    if (text == null || text.trim.isEmpty) {
+      throw new IllegalStateException("Document must have text.")
+    }
+  }
+
+  def determineClassification = {
+    val text = documentText.get.toLowerCase
+    
+    if (text.contains("low")) "Low"
+    else if (text.contains("medium")) "Medium"
+    else if (text.contains("high")) "High"
+    else "Unknown"
+  }
+
+  def isNotAttached = documentText.isEmpty
+  def isAttached = documentText.isDefined
+}
