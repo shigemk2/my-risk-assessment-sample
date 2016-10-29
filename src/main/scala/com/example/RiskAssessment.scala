f1c4f4434164574609a9054d18b0d16f015f1541
shigemk2 <i.am.shige@gmail.com>
2016-10-30 06:39:44 +0900

    implicit val timeout

--- src/main/scala/com/example/RiskAssessment.scala
+++ src/main/scala/com/example/RiskAssessment.scala
@@ -1,7 +1,13 @@
 package com.example
 
 import akka.actor._
+import akka.util.Timeout
 import com.example._
 
+import scala.concurrent.duration._
+
 object RiskAssessmentDriver extends CompletableApp(2) {
+  implicit val timeout = Timeout(5 seconds)
 }
+
+
